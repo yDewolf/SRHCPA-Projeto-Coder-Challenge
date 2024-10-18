e769eb0b806d6d3340f10f19e01d6e5452ffbf06
yDewolf <62114208+yDewolf@users.noreply.github.com>
2024-10-18 11:27:28 -0300

    File utils + Main csv reading functions

--- src/utils/fileUtils/CsvReader.py
+++ src/utils/fileUtils/CsvReader.py
@@ -1,1 +1,52 @@
 # CSV file reader
+import FileReader
+
+
+def get_csv_rows(file_path: str, header: list[str] = [], separator: str = ",") -> list[dict]:
+    file_lines = FileReader.get_file_lines(file_path)
+    
+    # Get header from file if a header wasn't inputted
+    if len(header) == 0:
+        # Header should be the first line of the file
+        header = file_lines[0].split(separator)
+
+    csv_rows = []
+
+    for line in file_lines:
+        # Skip header line
+        if line == file_lines[0]:
+            continue
+        
+        splitted_line = line.split(separator)
+
+        line_dict = {}
+        for column_idx, headerKey in enumerate(header):
+            # Header key = respective value in this line
+            line_dict[headerKey] = splitted_line[column_idx]
+        
+        csv_rows.append(line_dict)
+    
+    return csv_rows
+
+
+def get_csv_values_with_key(file_path: str, header: list[str] = [], main_key: str = "", separator: str = ",") -> dict:
+    raw_values = get_csv_rows(file_path, header, separator)
+    if main_key == "":
+        main_key = header[0]
+
+    indexed_csv = {}
+
+    for row_dict in raw_values:
+        filtered_dict = {}
+
+        for key in row_dict:
+            # Skip main key
+            if key == main_key:
+                continue
+            
+            filtered_dict[key] = row_dict[key]
+
+        # Index the value from row_dict that corresponds to the main key column as the filtered_dict
+        indexed_csv[row_dict[main_key]] = filtered_dict
+
+    return indexed_csv
--- src/utils/fileUtils/FileReader.py
+++ src/utils/fileUtils/FileReader.py
@@ -1,1 +1,29 @@
 # General file reading
+import os as OS
+
+def create_file(file_path: str, store_string: str = ""):
+    with open(file_path, "x") as file:
+        file.write(store_string)
+
+def save_file(file_path: str, data_as_str: str = "", overwrite: bool = True, auto_create: bool = True):
+    if overwrite:
+        if not OS.path.exists(file_path):
+            create_file(file_path, data_as_str)
+            return
+
+        with open(file_path, "w") as file:
+            file.write(data_as_str)
+        
+        return
+    
+    with open(file_path, "a") as file:
+        file.write(data_as_str)
+
+
+def get_file_lines(file_path: str) -> list:
+    lines = []
+    with open(file_path, "r") as file:
+        for line in file.readlines():
+            lines.append(line.replace("\n", ""))
+    
+    return lines
